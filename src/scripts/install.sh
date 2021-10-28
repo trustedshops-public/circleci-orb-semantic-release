0fe2cf9ab03a78a7bcc997c4a5e31f7fb18ab9f2
Timo Reymann <Timo.Reymann@trustedshops.de>
2021-10-28 11:52:51 +0200

    feat: Pin npm version

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,4 +1,4 @@
-sudo npm install --no-progress  -g semantic-release \
-  @semantic-release/git \
-  @semantic-release/changelog \
-  conventional-changelog-conventionalcommits
+sudo npm install --no-progress  -g semantic-release@18.0.0 \
+  @semantic-release/git@10.0.0 \
+  @semantic-release/changelog@6.0.0 \
+  conventional-changelog-conventionalcommits@4.6.1

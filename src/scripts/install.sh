2dc9ca9c8101ef1ba99f40d0426b19cad3bf8e5f
Timo Reymann <mail@timo-reymann.de>
2023-02-28 18:41:49 +0100

    chore: Move all packages to install on new line

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,10 +4,11 @@ else
     if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 fi
 
-$SUDO npm install --no-progress -g semantic-release@19.0.3 \
-  @semantic-release/git@10.0.1 \
-  @semantic-release/changelog@6.0.1 \
-  conventional-changelog-conventionalcommits@5.0.0
+$SUDO npm install --no-progress -g \
+    semantic-release@19.0.3 \
+    @semantic-release/git@10.0.1 \
+    @semantic-release/changelog@6.0.1 \
+    conventional-changelog-conventionalcommits@5.0.0
 
 if [ -n "$PARAM_ADDITIONAL_PACKAGES" ]
 then
